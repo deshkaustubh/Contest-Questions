af331727888d4bfd2b906fcfa30e98f108e2ec84
Kaustubh <ksd.tcet@gmail.com>
2025-02-06 20:30:10 +0530

    Initial Commit

---
+++ src/main/kotlin/codechef/solved/SmallPalindrome.kt
@@ -0,0 +1,22 @@
+package org.example.codechef.solved
+
+//https://www.codechef.com/problems/SMLPAL
+
+// x = one's
+// y = two's
+// both are even
+
+fun main() {
+    val t = readln().toInt()
+    repeat(t) {
+        val (noOfOnes, noOfTwos) = readln().split(" ").map {it.toInt()}
+        println(smallPalindrome(noOfOnes, noOfTwos))
+    }
+}
+
+fun smallPalindrome(noOfOnes: Int, noOfTwos: Int):String {
+    return  "1".repeat(noOfOnes / 2) +
+            "2".repeat(noOfTwos / 2) +
+            "2".repeat(noOfTwos / 2) +
+            "1".repeat(noOfOnes / 2)
+}
