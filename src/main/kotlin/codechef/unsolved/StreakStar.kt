554bda8083b28a20d180a1d0b5d3131467c27e4c
Kaustubh <ksd.tcet@gmail.com>
2025-02-07 10:32:00 +0530

    Initial Commit

---
+++ src/main/kotlin/codechef/unsolved/StreakStar.kt
@@ -0,0 +1,35 @@
+package org.example.codechef.unsolved
+
+fun main() {
+    val t = readln().toInt()
+    repeat(t) {
+        val (n, x) = readln().split(" ").map { it.toInt() }
+        val elements = readln().split(" ").map { it.toInt() }.toMutableList()
+        println(maxStreakValue(elements, x))
+    }
+}
+
+fun maxStreakValue(elements: MutableList<Int>, x: Int): Int {
+    var maxStreak = calculateStreak(elements)
+    for (i in elements.indices) {
+        val original = elements[i]
+        elements[i] *= x
+        maxStreak = maxOf(maxStreak, calculateStreak(elements))
+        elements[i] = original
+    }
+    return maxStreak
+}
+
+fun calculateStreak(arr: List<Int>): Int {
+    var maxStreakValue = 1
+    var currentStreakValue = 1
+    for (i in 1 until arr.size) {
+        if (arr[i] >= arr[i - 1]) {
+            currentStreakValue++
+        } else {
+            maxStreakValue = maxOf(maxStreakValue, currentStreakValue)
+            currentStreakValue = 1
+        }
+    }
+    return maxOf(maxStreakValue, currentStreakValue)
+}
