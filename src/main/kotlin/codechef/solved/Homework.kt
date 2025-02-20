7247a3be894ab07f227db0cf970484d21fe7307f
Kaustubh <ksd.tcet@gmail.com>
2025-02-20 22:49:54 +0530

    Initial Commit

--- src/main/kotlin/codechef/solved/Homework.kt
+++ src/main/kotlin/codechef/solved/Homework.kt
@@ -1,3 +1,20 @@
 package org.example.codechef.solved
 
 // [19-02-2025]
+
+fun main () {
+    val (x, y) = readln().split(" ").map { it.toInt() }
+    val result = homework(x, y)
+    println(result)
+}
+
+fun homework(x: Int , y: Int): String {
+
+    for(i in 0..10) {
+        if(x + (y * i) >= 100) {
+            return "yes"
+            break
+        }
+    }
+    return "no"
+}
