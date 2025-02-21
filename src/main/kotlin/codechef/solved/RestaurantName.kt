417fed5a33668be52aa14d9b59857f48b5a4d2ba
Kaustubh <ksd.tcet@gmail.com>
2025-02-21 11:09:07 +0530

    Initial Commit

--- src/main/kotlin/codechef/solved/RestaurantName.kt
+++ src/main/kotlin/codechef/solved/RestaurantName.kt
@@ -1,3 +1,29 @@
 package org.example.codechef.solved
 
 // [19-02-2025]
+
+// n -> length of name, all lowercase
+// a -> string, competitor
+// wants lexixographically larger than a both reverse and fwd
+
+import kotlin.random.Random
+
+fun main() {
+    val t = readln().toInt()
+    repeat(t) {
+        val n = readln().toInt()
+        val a = readln()
+        val result = lexicographically(n, a)
+        println(result)
+    }
+}
+
+fun lexicographically(n: Int, a: String): String {
+    if (a.all {it == 'z'}) return "-1"
+    var resultString = ""
+    for(i in 0 until n) {
+        val x = maxOf(a[i].toInt(), a[n-1-i].toInt())
+        resultString = resultString + (Random.nextInt(x,123)).toChar()
+    }
+    return resultString
+}
